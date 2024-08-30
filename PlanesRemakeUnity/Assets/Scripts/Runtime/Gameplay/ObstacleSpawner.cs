b3a74dc7ddea8e17271548e48cee03622d36a32d
ivano <bolivaritoviva@gmail.com>
2024-08-29 21:04:52 -0400

    ObstacleSpawner inheritance change
    - Made the ObstacleSpawner inherit the BaseSapwner class.

--- PlanesRemakeUnity/Assets/Scripts/Runtime/Gameplay/ObstacleSpawner.cs
+++ PlanesRemakeUnity/Assets/Scripts/Runtime/Gameplay/ObstacleSpawner.cs
@@ -2,51 +2,27 @@ namespace PlanesRemake.Runtime.Gameplay
 {
     using UnityEngine;
 
-    using PlanesRemake.Runtime.Utils;
-
-    //NOTE: Think about making this a generic class that can spawn all sort of objects.
-    public class ObstacleSpawner
+    public class ObstacleSpawner : BaseSpawner
     {
-        private readonly Quaternion obstacleDefaultRotation = Quaternion.Euler(new Vector3(-15, -15, 0));
-        
-        private Obstacle obstaclePrefab = null;
-        private Timer spawningTimer = null;
-        private CameraExtensions.Boundaries boundaries = default(CameraExtensions.Boundaries);
-        private Vector3 startingPosition = Vector3.zero;
+        protected override Vector3 StartingPosition => 
+            new Vector3(boundaries.right, boundaries.center.y, 0);
+        protected override Quaternion StartingRotation =>
+            Quaternion.Euler(new Vector3(-15, -15, 0));
+        protected override float SpawnDelayInSeconds => 30;
+        protected override bool SpawnPrefabOnCreation => true;
 
-        public ObstacleSpawner(Obstacle sourceObstaclePrefab, Camera sourceIsometricCamera)
+        public ObstacleSpawner(GameObject obstaclePrefab, Camera isometricCamera)
+            :base(obstaclePrefab, isometricCamera)
         {
-            obstaclePrefab = sourceObstaclePrefab;
-            
-            boundaries = sourceIsometricCamera.GetScreenBoundariesInWorld(Vector3.zero);
-            startingPosition = new Vector3(
-                boundaries.right,
-                boundaries.center.y,
-                0);
 
-            //We are calling this function once so that the first wall appears and we don't have the player 
-            //waiting for something to happen.
-            OnSpawningTimerCompleted();
-            spawningTimer = new Timer(30, sourceIsRepeating: true);
-            spawningTimer.OnTimerCompleted += OnSpawningTimerCompleted;
-            spawningTimer.Start();
         }
 
-        public void Dispose()
+        protected override GameObject SpawnPrefab(GameObject prefab)
         {
-            spawningTimer.OnTimerCompleted -= OnSpawningTimerCompleted;
-            spawningTimer.Stop();
-        }
-
-        private void SpawnObstacle(Obstacle obstaclePrefab)
-        {
-            Obstacle newObstacle = GameObject.Instantiate(obstaclePrefab);
-            newObstacle.Initialize(1, 10, boundaries.top, startingPosition, obstacleDefaultRotation);
-        }
-
-        private void OnSpawningTimerCompleted()
-        {
-            SpawnObstacle(obstaclePrefab);
+            GameObject obstacleGameObject = base.SpawnPrefab(prefab);
+            Obstacle newObstacle = obstacleGameObject.GetComponent<Obstacle>();
+            newObstacle.Initialize("Aircraft", 1, 10, boundaries.top, StartingPosition, StartingRotation);
+            return obstacleGameObject;
         }
     }
 }
