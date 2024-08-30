9d6b3aba35cd6b045b4b8cce7c48135cebf0d4e5
ivano <bolivaritoviva@gmail.com>
2024-08-29 21:07:31 -0400

    Obstacle detecting aircraft contacts
    - Made the the Obstacle class detect when an aircraft touches a wall or when it passes through a gap.
    - Send an event every time that the aircraft is detected during the two aforementioned cases.

--- PlanesRemakeUnity/Assets/Scripts/Runtime/Gameplay/Obstacle.cs
+++ PlanesRemakeUnity/Assets/Scripts/Runtime/Gameplay/Obstacle.cs
@@ -3,6 +3,7 @@ namespace PlanesRemake.Runtime.Gameplay
     using UnityEngine;
     
     using PlanesRemake.Runtime.Gameplay.CommonBehaviors;
+    using PlanesRemake.Runtime.Events;
 
     [RequireComponent(typeof(DirectionalMovement), typeof(OsilateMovement))]
     public class Obstacle : MonoBehaviour
@@ -15,6 +16,8 @@ namespace PlanesRemake.Runtime.Gameplay
         
         private DirectionalMovement directionalMovement = null;
         private OsilateMovement osilateMovement = null;
+        private bool wasCrossed = false;
+        private string triggerDetectionTag = string.Empty;
 
         #region Unity Methods
 
@@ -34,12 +37,23 @@ namespace PlanesRemake.Runtime.Gameplay
             gapCollider.OnTriggerExitDetected += OnGapTriggerExited;
         }
 
+        private void OnDestroy()
+        {
+            foreach(CollisionEventNotifier wallCollider in wallColliders)
+            {
+                wallCollider.OnTiggerEnterDetected -= OnWallTriggerEntered;
+            }
+
+            gapCollider.OnTriggerExitDetected -= OnGapTriggerExited;
+        }
+
         #endregion
 
         //NOTE: We can create a container that has all of this information so that it gets passed in
         //and we do not have this long list of parameters 
-        public void Initialize(float movementSpeed, float osilationSpeed, float osilationDistance, Vector3 startingPosition, Quaternion startingRotation)
+        public void Initialize(string triggerCollisionDetectionTag,float movementSpeed, float osilationSpeed, float osilationDistance, Vector3 startingPosition, Quaternion startingRotation)
         {
+            triggerDetectionTag = triggerCollisionDetectionTag;
             transform.position = startingPosition;
             transform.rotation = startingRotation;
             Vector3 velocityVector = new Vector3(-movementSpeed, 0, 0);
@@ -50,12 +64,23 @@ namespace PlanesRemake.Runtime.Gameplay
 
         private void OnWallTriggerEntered(Collider other)
         {
-            Debug.LogWarning("Wall collided!");
+            if(other.tag == triggerDetectionTag)
+            {
+                directionalMovement.enabled = false;
+                osilateMovement.enabled = false;
+                EventDispatcher.Instance.Dispatch(GameplayEvents.OnWallcollision, other);
+                Debug.LogWarning("Aircraft crashed");
+            }
         }
 
         private void OnGapTriggerExited(Collider other)
         {
-            Debug.LogWarning("Gap exited!");
+            if(other.tag == triggerDetectionTag && !wasCrossed)
+            {
+                wasCrossed = true;
+                EventDispatcher.Instance.Dispatch(GameplayEvents.OnGapCrossed, other);
+                Debug.LogWarning("Aircraft crossed wall");
+            }
         }
     }
 }
