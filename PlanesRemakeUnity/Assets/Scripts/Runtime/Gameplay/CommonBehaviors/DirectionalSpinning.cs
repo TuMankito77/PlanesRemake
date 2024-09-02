b85f5b1b8650e587dfc14e9c612c9ddc7e40f3fc
ivano <bolivaritoviva@gmail.com>
2024-09-02 11:54:35 -0400

    DirectionalSpinning adapation
    - Adapted the DirectionSpinning script to be more generic and changed it's name from SpinningPropeller to the one it currently has.

---
+++ PlanesRemakeUnity/Assets/Scripts/Runtime/Gameplay/CommonBehaviors/DirectionalSpinning.cs
@@ -0,0 +1,22 @@
+﻿namespace PlanesRemake.Runtime.Gameplay.CommonBehaviors
+{
+    using UnityEngine;
+
+    public class DirectionalSpinning : MonoBehaviour
+    {
+        [SerializeField]
+        private float spinningSpeed = 100;
+
+        [SerializeField]
+        private Vector3 rotatingDirection = Vector3.forward;
+
+        #region Unity Methods
+
+        void Update()
+        {
+            transform.Rotate(rotatingDirection, Time.deltaTime * spinningSpeed);
+        }
+
+        #endregion
+    }
+}
