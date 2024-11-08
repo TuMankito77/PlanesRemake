b0b1c036182e6ad5fa7599518d4200815ea5508d
ivano <bolivaritoviva@gmail.com>
2024-11-07 21:46:03 -0400

    Fuel collected clip addition
    - Added the fuel collection clip to the ClipsDatabase asset.

--- PlanesRemakeUnity/Assets/Scripts/Runtime/Sound/ClipIds.cs
+++ PlanesRemakeUnity/Assets/Scripts/Runtime/Sound/ClipIds.cs
@@ -9,6 +9,7 @@ namespace PlanesRemake.Runtime.Sound
 		public const string BUTTON_CLICK_CLIP = "ButtonClickClip";
 		public const string AIRCRAFT_ENGINE_CLIP = "AircraftEngineClip";
 		public const string AIRCRAFT_EXPLOSION_CLIP = "AircraftExplosionClip";
+		public const string FUEL_CLIP = "FuelClip";
 	}
 }
 
