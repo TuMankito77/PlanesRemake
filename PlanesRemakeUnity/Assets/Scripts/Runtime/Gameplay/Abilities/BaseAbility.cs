760195b18f8f8982b47a15e3948eec2eb0fa1c61
ivano <bolivaritoviva@gmail.com>
2025-01-04 07:50:15 -0400

    Abilities implementation start commit
    - Added a new base class that will be in charge of handling abilities for the player.

---
+++ PlanesRemakeUnity/Assets/Scripts/Runtime/Gameplay/Abilities/BaseAbility.cs
@@ -0,0 +1,55 @@
+namespace PlanesRemake.Runtime.Gameplay.Abilities
+{
+    using System;
+    
+    using UnityEngine;
+    
+    using PlanesRemake.Runtime.Utils;
+
+    public abstract class BaseAbility
+    {
+        public event Action onAbilityEffectFinished;
+
+        protected GameObject owner = null;
+
+        private Timer activeTimer = null;
+
+        protected abstract bool IsAbilityTimerTickEnabled { get; }
+
+        public BaseAbility(GameObject sourceOwner, float sourceDurationInSeconds)
+        {
+            owner = sourceOwner;
+            activeTimer = new Timer(sourceDurationInSeconds);
+        }
+
+        public virtual void Activate()
+        {
+            if(activeTimer.IsRunning)
+            {
+                activeTimer.Restart();
+                return;
+            }
+
+            activeTimer.OnTimerCompleted += Deactivate;
+            activeTimer.OnTimerCompleted += onAbilityEffectFinished;
+
+            if(IsAbilityTimerTickEnabled)
+            {
+                activeTimer.OnTimerTick += OnAbilityTimerTick;
+            }
+
+            activeTimer.Start();
+        }
+
+        public virtual void Deactivate()
+        {
+            activeTimer.Stop();
+            activeTimer.OnTimerCompleted -= Deactivate;
+        }
+
+        protected virtual void OnAbilityTimerTick(float deltaTime, float timeTranscurred)
+        {
+
+        }
+    }
+}
